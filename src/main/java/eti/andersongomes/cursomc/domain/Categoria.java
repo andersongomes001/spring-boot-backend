6d7258bbebb67e32c4b4053a59596fd0a7083af7
Anderson <comprasgomes@hotmail.com>
2018-02-08 21:21:19 -0200

    testando a primeira classe de dominio

---
+++ src/main/java/eti/andersongomes/cursomc/domain/Categoria.java
@@ -0,0 +1,50 @@
+package eti.andersongomes.cursomc.domain;
+
+import java.io.Serializable;
+
+public class Categoria implements Serializable {
+    private static final long serialVersionUID = 1l;
+
+    private Integer id;
+    private String nome;
+
+    public Categoria() {
+    }
+
+    public Categoria(Integer id, String nome) {
+        super();
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Categoria categoria = (Categoria) o;
+
+        return id != null ? id.equals(categoria.id) : categoria.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
