afaf335493fa2159bacf70d29589535f7fd44dfd
Anderson <comprasgomes@hotmail.com>
2018-02-08 21:44:18 -0200

    banco de dados h2 e criaçã automatica da base de dados

--- src/main/java/eti/andersongomes/cursomc/domain/Categoria.java
+++ src/main/java/eti/andersongomes/cursomc/domain/Categoria.java
@@ -1,10 +1,19 @@
 package eti.andersongomes.cursomc.domain;
 
+import jdk.nashorn.internal.runtime.logging.Logger;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
+@Entity
 public class Categoria implements Serializable {
     private static final long serialVersionUID = 1l;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
