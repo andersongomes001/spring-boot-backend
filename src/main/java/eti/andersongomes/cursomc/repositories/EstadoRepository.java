e0b03a1b1a29b5630ac202cdbb9e156fbb4f4b4e
Anderson <comprasgomes@hotmail.com>
2018-02-09 21:11:50 -0200

    estados e cidades

---
+++ src/main/java/eti/andersongomes/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,10 @@
+package eti.andersongomes.cursomc.repositories;
+
+import eti.andersongomes.cursomc.domain.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
