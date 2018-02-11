76ee75f689411c1c97da21f7f003de1fbca033d7
Anderson <comprasgomes@hotmail.com>
2018-02-11 18:54:02 -0200

    Validação customizada: email não repetido na atualização de Cliente

--- src/main/java/eti/andersongomes/cursomc/dto/ClienteDTO.java
+++ src/main/java/eti/andersongomes/cursomc/dto/ClienteDTO.java
@@ -1,12 +1,14 @@
 package eti.andersongomes.cursomc.dto;
 
 import eti.andersongomes.cursomc.domain.Cliente;
+import eti.andersongomes.cursomc.services.validation.ClienteUpdate;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import java.io.Serializable;
 
+@ClienteUpdate
 public class ClienteDTO  implements Serializable {
     private static final long serialVersionUID = 1L;
 
