060a8b0e82553afeb52ed2bb8d3612e64075ae70
Anderson <comprasgomes@hotmail.com>
2018-02-11 17:42:36 -0200

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/eti/andersongomes/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/eti/andersongomes/cursomc/services/validation/ClienteInsertValidator.java
@@ -1,9 +1,12 @@
 package eti.andersongomes.cursomc.services.validation;
 
+import eti.andersongomes.cursomc.domain.Cliente;
 import eti.andersongomes.cursomc.domain.enums.TipoCliente;
 import eti.andersongomes.cursomc.dto.ClienteNewDTO;
+import eti.andersongomes.cursomc.repositories.ClienteRepository;
 import eti.andersongomes.cursomc.resources.exception.FieldMessage;
 import eti.andersongomes.cursomc.services.validation.utils.BR;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
@@ -11,12 +14,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+    @Autowired
+    private ClienteRepository repo;
 
     @Override
     public void initialize(ClienteInsert ann) {
     }
     @Override
     public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+
         List<FieldMessage> list = new ArrayList<>();
         // inclua os testes aqui, inserindo erros na lista
         if(objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getCpfOuCnpj())){
@@ -25,6 +31,12 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
         if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())){
             list.add(new FieldMessage("CpfOuCnpj", "CNPJ inválido"));
         }
+
+        Cliente aux = repo.findByEmail(objDto.getEmail());
+        if(aux != null){
+            list.add(new FieldMessage("email", "EMAIL já existe"));
+        }
+
         for (FieldMessage e : list) {
             context.disableDefaultConstraintViolation();
             context.buildConstraintViolationWithTemplate(e.getMessage())
