f2ecf3e2eb1c3df0cfd610e637bafa35e2ca4ba7
Anderson <comprasgomes@hotmail.com>
2018-02-11 09:53:03 -0200

    personalizando o retorno da validação

--- src/main/java/eti/andersongomes/cursomc/resources/exception/StandardError.java
+++ src/main/java/eti/andersongomes/cursomc/resources/exception/StandardError.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import java.io.Serializable;
 
 public class StandardError implements Serializable {
-    private static final long serialVersionUID = 1l;
+    private static final long serialVersionUID = 1L;
 
     private Integer status;
     private String msg;
